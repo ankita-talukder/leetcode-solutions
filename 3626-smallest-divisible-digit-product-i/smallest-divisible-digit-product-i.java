7957547ff7900783cfdc9e76d772b99125054077
ankita-talukder <ankitatalukder2005@gmail.com>
2026-08-06 09:13:41 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (30.89%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int number = n;
+        while(true){
+            if(isDivisible(number , t)){
+                return number;
+            }
+            number++;
+        }
+    }
+    private boolean isDivisible(int number,int t){
+        int product = 1;
+
+        while(number > 0){
+            int digit = number % 10;
+            product *= digit;
+            number /= 10;
+        }
+        return product % t == 0;
+    }
+}
